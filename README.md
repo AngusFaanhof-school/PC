a4b8e3b9b6f0ed36377e197317cdcecde57936da
AngusFaanhof <github@adfpm.anonaddy.com>
2021-12-02 11:18:53 +0100

    added robot move functionality

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Group project for Pervasive Computing
+
+## Lego mindstorm add-on
+MATLAB Support Package for LEGO MINDSTORMS EV3 Hardware
+
+## Lego mindstorm matlab documentation
+https://www.mathworks.com/help/supportpkg/legomindstormsev3io/referencelist.html?type=function
--- code.m
+++ code.m
@@ -1,1 +1,39 @@
-% code
+robot = legoev3('usb');
+
+leftMotor = motor(robot, 'A');
+rightMotor = motor(robot, 'B');
+
+sonar = sonicSensor(robot);
+
+moveSpeed = 30;
+leftMotor.Speed = moveSpeed;
+rightMotor.Speed = moveSpeed;
+
+start(leftMotor);
+start(rightMotor);
+
+while(1)
+    distance = readDistance(sonar);
+
+    command = "n";
+    if (distance < 0.2)
+        stop(leftMotor);
+        stop(rightMotor);
+        command = input("what?", 's'); % Process the image
+    end
+
+    if (command == "n")
+        start(leftMotor);
+        start(rightMotor);
+    elseif (command == "l")
+        stop(leftMotor);
+        start(rightMotor);
+        pause(1.63);
+    elseif (command == "r")
+        start(leftMotor);
+        stop(rightMotor);
+        pause(1.63);
+    else
+        pause(0.01);
+    end
+end
