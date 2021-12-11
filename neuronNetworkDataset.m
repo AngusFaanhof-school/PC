acde5268962b0ca4d521cba5cc7b45e355e63824
PawoOoOo <49211836+Pawopiiieee@users.noreply.github.com>
2021-12-11 19:48:13 +0100

    Update neuronNetwork, test and train data

--- neuronNetworkDataset.m
+++ neuronNetworkDataset.m
@@ -1,13 +1,5 @@
-testData  = csvread('testImages.csv');
-trainData  = csvread('trainImages.csv');
-
-for k=1:192
-    j = testData(1:end, k);
-    length(j)
-    digit = reshape(j,[28,28]);
-    subplot(16,12,k)
-    imshow(digit);
-end
+x  = csvread('testImages.csv');
+t  = csvread('trainImages.csv');
 
 trainFcn = 'trainscg';  % Scaled conjugate gradient backpropagation.
 
@@ -21,13 +13,13 @@ net.divideParam.valRatio = 15/100;
 net.divideParam.testRatio = 15/100;
 
 % Train the Network
-[net,tr] = train(net,testData,trainData);
+[net,tr] = train(net,x,t);
 
 % Test the Network
-y = net(testData);
-e = gsubtract(trainData,y);
-performance = perform(net,trainData,y)
-tind = vec2ind(trainData);
+y = net(x);
+e = gsubtract(t,y);
+%performance = perform(net,t,y)
+tind = vec2ind(t);
 yind = vec2ind(y);
 percentErrors = sum(tind ~= yind)/numel(tind);
 
@@ -41,4 +33,6 @@ view(net)
 %figure, ploterrhist(e)
 %figure, plotconfusion(t,y)
 %figure, plotroc(t,y)
-
+netSave = net
+filename = 'trainNN';
+save(filename, 'netSave')
--- putImInMtrx.m
+++ putImInMtrx.m
@@ -5,12 +5,7 @@ trainFile = [];
 for k=1:length(Files)
     Filename = Files(k).name;
     im = imread(Filename); 
-    level=graythresh(im)
-    im4=im2bw(im,0.5); 
-    im5=imopen(im4,strel('disk',2)); 
-    im6=imcomplement(im5);
-    vect = im6(:);
-    
+    vect = im(:);
     output = [
         startsWith(Filename, 'l'); %turnLeft
         startsWith(Filename, 'r'); %turnRight
@@ -20,6 +15,6 @@ for k=1:length(Files)
     trainFile = [trainFile output];
 end
 
-writematrix(testFile, 'test.csv');
-writematrix(trainFile, 'train.csv');
+writematrix(testFile, 'testHandIm.csv');
+writematrix(trainFile, 'trainHandIm.csv');
 
